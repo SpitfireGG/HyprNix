686e38abe19610f3b6c09584580344a1a2174048
SpitfireGG <raambasnet02@gmail.com>
2024-04-26 22:25:19 +0545

    flakies

--- nixOS/nix-config/flake.nix
+++ nixOS/nix-config/flake.nix
@@ -1,45 +1,39 @@
 {
-  description = "nix flake config for single system";
+  description = "Kenzo's personal dotfile";
 
   inputs = {
-
-    stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "unstable";
-    nixpkgs.follows = "unstable";
-
-    ags.url = "github:Aylur/ags";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-23.11";
+    hm.url = "github:nix-community/home-manager/master";
+    nix-colors.url = "github:misterio77/nix-colors";
+    anyrun.url = "github:Kirottu/anyrun";
+    ags.url = "github:ozwaldorf/ags";
+    nur.url = "github:nix-community/NUR";
+    spicetify-nix.url = "github:the-argus/spicetify-nix";
+    nixvim.url = "github:elythh/nixvim";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
 
-      # overlays = import ./overlays { inherit inputs; };
-      # nixosModules = import ./modules/nixos;
-      # homeManagerModules = import ./modules/home-manager;
-
-      nixosConfigurations = {
-        kenzo = nixpkgs.lib.nixosSystem {
-          system = system;
-          specialArgs = { inherit inputs; };
-          modules = [ ./nixos/configuration.nix ];
-        };
+ outputs = { self, nixpkgs, nixpkgs-stable, hm, ... }@inputs: {
+    nixosConfigurations = {
+      kenzo = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+        ];
       };
-
-      homeConfigurations = {
-        "kenzo@kenzo" = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          extraSpecialArgs = { inherit inputs outputs self; };
-          modules = [ ./home-manager/home.nix ];
-        };
-      };
-      kenzo = self.nixosConfigurations.kenzo.config.system.build.toplevel;
     };
+    homeConfigurations = {
+      kenzo = hm.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs-stable {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+        };
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./home-manager/home.nix ];
+      };
+    };
+      kenzo = self.nixosConfigurations.kenzo.config.system.build.toplevel;
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+ };
 }
-
