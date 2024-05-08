7d337c4c9381aad95dab16778d0ea7364205292c
SpitfireGG <raambasnet02@gmail.com>
2024-05-08 22:34:06 +0545

    per lang setups

---
+++ nvim/lua/plugins/configs/personalDevelopmentEnv/css-intel.lua
@@ -0,0 +1,19 @@
+local statOk, css = pcall(require, "html-css")
+if not statOk then
+    return
+end
+
+css.setup({
+    max_count = {}, -- not ready yet
+    enable_on = {
+        "html",
+        "css",
+        "tsx",
+    },                                                   -- set the file types you want the plugin to work on
+    file_extensions = { "css", "sass", "less", "html" }, -- set the local filetypes from which you want to derive classes
+    style_sheets = {
+        -- example of remote styles, only css no js for now
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css",
+    },
+})
