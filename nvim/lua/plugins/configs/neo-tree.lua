e5cd45fcb8d8cee6d507b63460f058dacebefa05
SpitfireGG <raambasnet02@gmail.com>
2024-05-08 22:31:31 +0545

    explorer

---
+++ nvim/lua/plugins/configs/neo-tree.lua
@@ -0,0 +1,75 @@
+local status_ok, neo_tree = pcall(require, "neo-tree")
+if not status_ok then
+	vim.notify("Neotree not found")
+	return
+end
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+neo_tree.setup({
+	close_if_last_window = true,
+	enable_diagnostics = false,
+	source_selector = {
+		winbar = true,
+		content_layout = "center",
+		tab_labels = {
+			filesystem = " File",
+			buffers = " Buffs",
+			git_status = " Git",
+			diagnostics = "",
+		},
+	},
+	default_component_configs = {
+		indent = {
+			padding = 0,
+		},
+		icon = {
+			folder_closed = "",
+			folder_open = "",
+			folder_empty = "",
+		},
+		git_status = {
+			symbols = {
+				unstaged = "✗",
+				staged = "✓",
+				unmerged = "",
+				renamed = "➜",
+				untracked = "★",
+				deleted = "",
+				ignored = "",
+			},
+		},
+	},
+	window = {
+		width = 30,
+		mappings = {
+			["<space>"] = {
+				"toggle_node",
+				nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+			},
+			["o"] = "open",
+			["v"] = "open_vsplit",
+			["C"] = "close_node",
+			["z"] = "close_all_nodes",
+			["Z"] = "expand_all_nodes",
+		},
+	},
+	filesystem = {
+		follow_current_file = true,
+
+		hijack_netrw_behavior = "open_current",
+		use_libuv_file_watcher = true,
+		filtered_items = {
+			visible = true,
+			hide_dotfiles = false,
+			hide_gitignored = true,
+		},
+	},
+	event_handlers = {
+		{
+			event = "neo_tree_buffer_enter",
+			handler = function(_)
+				vim.opt_local.signcolumn = "auto"
+			end,
+		},
+	},
+})
