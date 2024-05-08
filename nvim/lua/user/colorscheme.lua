a7e4906f605731813e29927442580150aea1ea12
SpitfireGG <raambasnet02@gmail.com>
2024-05-08 22:39:51 +0545

    load colorscheme

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = "gruvbox"
+-- vim.o.background = "light"
+
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
+end
