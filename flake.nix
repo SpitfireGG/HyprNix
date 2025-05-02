540e3cb65425e459b65a37f2eaa0f39a8026485f
SpitfireGG <raambasnet02@gmail.com>
2025-05-02 18:44:37 +0545

    remove redundant inputs

--- flake.nix
+++ flake.nix
@@ -1,43 +1,48 @@
-
 {
   description = "Kenzo's personal flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    hm = { 
-          url =   "github:nix-community/home-manager";
-          inputs.nixpkgs.follows = "nixpkgs";
+    hm = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-colors.url = "github:misterio77/nix-colors";
-    anyrun.url = "github:Kirottu/anyrun";
     ags.url = "github:ozwaldorf/ags";
-    nur.url = "github:nix-community/NUR";
     spicetify-nix.url = "github:the-argus/spicetify-nix";
     nixvim.url = "github:elythh/nixvim";
   };
 
-
- outputs = { self, nixpkgs,  hm, ... }@inputs: {
-    nixosConfigurations = {
-      kenzo = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-	specialArgs = { inherit inputs; };
-        modules = [
-          ./nixos/configuration.nix
-        ];
-      };
-    };
-    homeConfigurations = {
-      kenzo = hm.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      hm,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        kenzo = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          config = { allowUnfree = true; };
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/dell/configuration.nix
+            ./modules/nixos/ciscoPacketTracer/packetTracerByp.nix
+          ];
         };
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home/home.nix ];
       };
-    };
+      homeConfigurations = {
+        kenzo = hm.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = {
+              allowUnfree = true;
+            };
+          };
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ./kenzo/home.nix ];
+        };
+      };
       kenzo = self.nixosConfigurations.kenzo.config.system.build.toplevel;
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
- };
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    };
 }
