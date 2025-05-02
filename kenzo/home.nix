576d0d9fb6db1aebe4f1cd997c9afb80a07a59c4
SpitfireGG <raambasnet02@gmail.com>
2025-05-02 19:52:29 +0545

    feat: modularize hm

--- kenzo/home.nix
+++ kenzo/home.nix
@@ -1,4 +1,10 @@
-{ inputs, config, pkgs, lib, ... } @ args:
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
+}@args:
 
 let
   username = "kenzo";
@@ -6,91 +12,20 @@ let
   system = pkgs.system; # Get system from pkgs context
 in
 {
-  imports = [ 
+  imports = [
+    ../modules/home-manager/programs/default.nix
+    ../modules/home-manager/user/program.nix
+    ../modules/home-manager/user/theming.nix
   ];
 
   home = {
     inherit username homeDirectory;
     stateVersion = "24.11";
 
-    packages = with pkgs; [
-      inputs.nixvim.packages.${system}.default
-      phinger-cursors
-      gruvbox-gtk-theme
-      reversal-icon-theme
-
-      bc
-      qbittorrent
-      caprine-bin
-      vesktop
-      firefox-unwrapped
-      wl-clipboard
-      unrar
-      sway-contrib.grimshot
-      glib
-      htop
-      gcc
-      nchat
-      wget
-      go
-      gopls
-      playerctl
-      mpc-cli
-      neovim
-      xdg-desktop-portal
-      imagemagick
-      xorg.xev
-      procps
-      gammastep
-      moreutils
-      cava
-      mpdris2
-      pavucontrol
-      feh
-	xbanish
-    ];
-
     sessionVariables = {
       XCURSOR_THEME = "phinger-cursors";
       XCURSOR_SIZE = "24";
       GTK_THEME = "Gruvbox-dark";
-    };
-  };
-
-  programs = {
-    home-manager.enable = true;
-
-    git = {
-      enable = true;
-      userName = "SpitfireGG";
-      userEmail = "raambasnet02@gmail.com";
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.rebase = true;
-      };
-    };
-
-    bash.enable = true;
-    direnv.enable = true;
-    fzf.enable = true;
-  };
-
-  gtk = {
-    enable = true;
-    theme = {
-      package = pkgs.gruvbox-gtk-theme;
-      name = "Gruvbox-dark"; 
-    };
-    iconTheme = {
-      package = pkgs.reversal-icon-theme;
-      name = "Reversal-dark"; 
-    };
-    cursorTheme = {
-      package = pkgs.phinger-cursors;
-      name = "phinger-cursors";
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
     };
   };
 
@@ -105,6 +40,6 @@ in
   };
 
   fonts.fontconfig.enable = true;
-  
+
   systemd.user.startServices = "sd-switch";
 }
