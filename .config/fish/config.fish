c6b5c6fad3e0481094563ffbb5972ef7f99f0bac
SpitfireGG <raambasnet02@gmail.com>
2025-05-26 22:32:41 +0545

    add git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,23 +10,12 @@ export PATH="$HOME/go/bin:$PATH"
 set -x GOPATH $HOME/go
 set -x PATH $PATH /usr/bin/cargo
 
-
-
-
-
-
 #-----------aliases----------------
-
-
-
-
-
-
 #Syntax highlighting for manpage - better understanding :)
 # can also be achieved by using Pager 
 
-function shit
-    man $argv | bat --plain --language=man
+function vv
+    man $argv[1] $argv[2] | bat --plain --language=man
 end
 
 
@@ -34,7 +23,33 @@ function copy
   find $argv[1] -type f -name $argv[2] | tee /dev/tty | xclip  -selection clipboard 
 end 
 
+function noc
+  set -l command_string (string join " " $argv)
 
+  echo "Running: $command_string" # Optional: show the user the command being run
+
+  command $argv
+
+  set -l exit_status $status
+
+  if $exit_status -eq 0
+    # Success (exit status 0)
+    dunstify \
+      -u low \
+      -i emblem-ok \
+      "Success" \
+      "'$command_string' completed successfully."
+  else
+    # Failure (non-zero exit status)
+    dunstify \
+      -u critical \
+      -i emblem-error \
+      "Failed ($exit_status)" \
+      "'$command_string' exited with status $exit_status."
+  end
+
+  return $exit_status
+end
 
 
 
@@ -46,15 +61,41 @@ alias fs 'source ~/.config/fish/config.fish'
 alias bb 'cd ../../'
 alias fuck 'nvim $(fzf --preview="bat {}")'
 
-#--------------git aliases----------------
+#-------------- git aliases ----------------
+# -- functions --
+function gstt
+    git status --short | fzf --multi | awk '{print $2}' | xargs git add
+      git status
+end
+
+function gswitch
+  git checkout $argv[1] &&  nvim $(find . -type f -name '*.asm' | fzf)
+
+end
+
+
+
+#----------------- end -------------------------
 alias gst 'git status .'
 alias gp 'git push -u origin'
+alias gpm 'git push -u origin main'
 alias gll 'git log -1 HEAD --stat'
 alias gc 'git commit -m'
 alias grv 'git remote -v'
 alias glog "git log --graph --pretty=format:'%C(red)%h%Creset -%C(yellow)%d%Creset %s %C(green)(%cr)%C(yellow)<%an>%Creset'"
-alias gch 'git checkout'
+alias gco 'git checkout'
+alias gcb 'git checkout'
 alias gua 'git reset'
 alias gitmad 'git reset --hard HEAD'
+
+# -- fast branch switching -- 
+# --- project dependent ---
+alias gbr='git branch -v'
+alias gbf='git branch | fzf | xargs git checkout'  # Interactive branch selection with fzf
+alias gch01='git checkout ch01'
+alias gchasm='git checkout x86_64_intel_guide'
+
+
+
 
 starship init fish | source
